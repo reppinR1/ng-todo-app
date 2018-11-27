9a3db1dc02bc19aa33d0bf0fa46744a1946ee5d1
Piotr Kula <piotrpawelkula@gmail.com>
2018-11-27 20:21:10 +0100

    can add todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  id: number;
+  content: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input [(ngModel)]="inputValue" (keydown.enter)="addTodo()" />
+    <button (click)="addTodo()" [disabled]="!inputValue">Add</button>
+    <ul>
+      <li *ngFor="let todo of todos">{{ todo.content }}</li>
+    </ul>
+  `,
+  styles: [
+    `
+      ul {
+        list-style: none;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  inputValue = '';
+  id = 0;
+  todos: Todo[] = [
+    {
+      id: ++this.id,
+      content: 'Wake up',
+      completed: true
+    }
+  ];
+
+  addTodo() {
+    if (!this.inputValue) {
+      return;
+    }
+    this.todos.push({
+      id: ++this.id,
+      content: this.inputValue,
+      completed: false
+    });
+    this.inputValue = '';
+  }
 }
