1149cf7b97c20ac7683e887d1d33c9dd7b97ee41
Piotr Kula <piotrpawelkula@gmail.com>
2018-11-29 00:25:09 +0100

    add css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,68 @@ import { TodosService } from './todos.service';
 @Component({
   selector: 'app-root',
   template: `
-    <input [(ngModel)]="service.inputValue" (keydown.enter)="service.addTodo()" />
-    <button (click)="service.addTodo()" [disabled]="!service.inputValue">Add</button>
-    <ul>
-      <li *ngFor="let todo of service.todos">
-        <span [class.completed]="todo.completed">{{ todo.content }} </span>
-        <button (click)="service.toggleTodo(todo.id)">Toggle</button>
-        <button (click)="service.removeTodo(todo.id)">Remove</button>
-      </li>
-    </ul>
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          [(ngModel)]="service.inputValue"
+          (keydown.enter)="service.addTodo()"
+        />
+        <button (click)="service.addTodo()" [disabled]="!service.inputValue">
+          Add
+        </button>
+        <div></div>
+        <ul>
+          <li *ngFor="let todo of service.todos">
+            <button (click)="service.toggleTodo(todo.id)" class="edit-button">
+              -
+            </button>
+            <button (click)="service.removeTodo(todo.id)" class="edit-button">
+              X
+            </button>
+            <span [class.completed]="todo.completed">{{ todo.content }} </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   `,
   styles: [
     `
-      ul {
-        list-style: none;
+      :host {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        min-height: 100vh;
+        font-family: Helvetica;
+        background: #f5f5f5;
       }
 
+      h1 {
+        color: #369;
+      }
+      ul {
+        list-style: none;
+        display: inline;
+      }
+
+      li {
+        margin-top: 8px;
+        font-size: 24px;
+      }
+      input,
+      button {
+        height: 2rem;
+      }
+
+      input {
+        margin-right: 8px;
+        font-size: 24px;
+      }
+      .edit-button {
+        margin-right: 8px;
+        width: 24px;
+      }
       .completed {
         text-decoration: line-through;
       }
